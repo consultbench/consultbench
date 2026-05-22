193f8a85da3a3bde46cf67452d2d529b5235ed66
ConsultBench <noreply@consultbench.github.io>
2026-05-22 16:05:31 -0700

    Use trailing slash static routes

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const basePath = process.env.NEXT_PUBLIC_SITE_BASE_PATH || "";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  trailingSlash: true,
   outputFileTracingRoot: path.join(__dirname),
   basePath,
   assetPrefix: basePath || undefined,
